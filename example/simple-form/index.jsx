cede2689323d90998d1f89e38be5e7cd808c9a01
Damian Aruj <aruj.damian@gmail.com>
2017-01-02 17:11:56 -0300

    removed test package from package.json and updated examples to point to the local jsx files

--- example/simple-form/index.jsx
+++ example/simple-form/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component }    from 'react';
 import ReactDOM                from 'react-dom';
-//import Form                    from '../../dist';
-import Form                    from 'react-awesome-form-validator';
+import Form                    from '../../src/form';
 import { isAlpha, isEmail }    from 'validator';
 
 class Root extends Component {

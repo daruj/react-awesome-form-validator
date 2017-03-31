30d36857036690b5ab75cfb3e88272f8fedb508b
Damian Aruj <aruj.damian@gmail.com>
2017-03-30 23:32:44 -0300

    working on the improvement of the build process

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,69 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const exampleFolder = process.env.exampleFolder;
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
+const PRODUCTION = process.env.NODE_ENV === 'production';
 
-const webpackConfig = {
-  entry: {
-    app: [
-      'babel-polyfill', // Set up an ES6-ish environment
-      'react-hot-loader/patch',
-      'webpack-dev-server/client?http://localhost:3000/', // WebpackDevServer host and port
-      `./example/${exampleFolder}/index.jsx`
-    ],
-    vendor: './src/vendors/index.js'
-  },
-  output: {
+const plugins = [
+  new webpack.NoEmitOnErrorsPlugin(),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    // this assumes your vendor imports exist in the node_modules directory
+    minChunks: (module) => module.context && module.context.indexOf('node_modules') !== -1
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      DEVELOPMENT: JSON.stringify(DEVELOPMENT),
+      PRODUCTION: JSON.stringify(PRODUCTION)
+    }
+  }),
+  new ExtractTextPlugin({
+    filename: 'bundle-[hash].css',
+    disable: false,
+    allChunks: true
+  }),
+  new webpack.NamedModulesPlugin()
+];
+
+if (PRODUCTION) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
+} else {
+  plugins.push(
+    new HtmlWebpackPlugin({
+      template: `./example/${exampleFolder}/index.template.html`
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  );
+}
+
+const entry = PRODUCTION
+  ? {
+    main: './src/form/index.jsx'
+  } :
+{
+  main: [
+    'babel-polyfill', // Set up an ES6-ish environment
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000/', // WebpackDevServer host and port
+    `./example/${exampleFolder}/index.jsx`
+  ]
+};
+
+const output = PRODUCTION
+  ? {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle-[name].js',
+    publicPath: '/'
+  } : {
     path: path.resolve(__dirname, './build/dev_build'),
     filename: 'bundle-[name]-[hash].js',
     publicPath: '/'
-  },
+  };
+
+const webpackConfig = {
+  entry,
+  output,
   devtool: 'eval',
   devServer: {
     historyApiFallback: true,
@@ -62,29 +109,7 @@ const webpackConfig = {
       react: path.join(__dirname, 'node_modules/react')
     }
   },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'bundle-[name]-[hash].js',
-      minChunks: Infinity
-    }),
-    new HtmlWebpackPlugin({
-      template: `./example/${exampleFolder}/index.template.html`
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development')
-      }
-    }),
-    new ExtractTextPlugin({
-      filename: 'bundle-[hash].css',
-      disable: false,
-      allChunks: true
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+  plugins
 };
 
 
